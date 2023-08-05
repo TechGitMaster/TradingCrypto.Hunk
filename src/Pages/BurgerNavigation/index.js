ae90701d99bb3a6591d3f4fa43f517a751f6e744
Kyle Matthew Velarde <kylevelarde374@gmail.com>
2023-08-05 11:56:52 +0800

    Final update

--- src/Pages/BurgerNavigation/index.js
+++ src/Pages/BurgerNavigation/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import { Logo, RocketGIF } from '../../utilities/PNG';
 
 const BurgerNavigation = ({ clickNavi, setBurgerCon }) => {
